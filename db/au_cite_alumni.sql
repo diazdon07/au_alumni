d4326919f3954f53002b3d520c6327217bbc2899
diazdon07 <donmclind.diaz@gmail.com>
2024-01-04 16:54:01 +0800

    db commit

--- db/au_cite_alumni.sql
+++ db/au_cite_alumni.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 04, 2024 at 09:51 AM
+-- Generation Time: Jan 04, 2024 at 09:53 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
